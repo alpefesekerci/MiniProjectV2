b09c58f7ba887c16857d2bbd8b794513054aeaae
Alp Efe Şekerci <alpefesekerci@gmail.com>
2026-03-31 10:08:47 +0300

    StudentRepositery kısmı ArrayList yerine HashMap ile değiştirildi.
    ArrayList yerine HashMap kullanılarak ID bazlı veri erişim performansı O(1) seviyesine çekildi. Döngüler kaldırılarak algoritma optimize edildi.

--- src/com/mini/project/v2/repository/StudentRepository.java
+++ src/com/mini/project/v2/repository/StudentRepository.java
@@ -1,40 +1,45 @@
 package com.mini.project.v2.repository;
 
 import com.mini.project.v2.model.Student;
+import com.mini.project.v2.util.FileUtil;
 
+import java.util.HashMap;
 import java.util.List;
-import java.util.Iterator;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class StudentRepository {
-    private final List<Student> studentList = new ArrayList<>();
+    private final Map<Integer, Student> studentMap = new HashMap<>();
 
     public boolean existsById(int id) {
-        for (Student s : studentList) {
-            if (s.getId() == id) {
-                return true;
-            }
-        }
-        return false;
+        return studentMap.containsKey(id);
     }
 
     public void save(Student student) {
-        studentList.add(student);
+        studentMap.put(student.getId(), student);
+    }
+
+    public Student findById(int id) {
+        return studentMap.get(id);
     }
 
     public List<Student> findAll() {
-        return studentList;
+        return new ArrayList<>(studentMap.values());
     }
 
     public void deleteById(int id) {
-        Iterator<Student> iterator = studentList.iterator();
-        while (iterator.hasNext()) {
-            Student student = iterator.next();
-            if (student.getId() == id) {
-                iterator.remove();
-                return;
-            }
-        }
+        studentMap.remove(id);
     }
 
+    public void loadData() {
+        List<Student> loadedStudents = FileUtil.readStudentsFromFile();
+        loadedStudents.forEach(student -> studentMap.put(student.getId(), student));
+    }
+
+    public void saveData() {
+        FileUtil.writeStudentsToFile(new ArrayList<>(studentMap.values()));
+    }
 }
+
+
+
